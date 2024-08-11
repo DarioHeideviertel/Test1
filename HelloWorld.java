f1fb1e840ce9dbff0a28ec3efed35006b99dffa7
DarioElpapi <dario.carballeira98@web.de>
2024-08-11 13:40:24 +0200

    klassisches HW

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,19 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Veränderung!");
-        //kleiner kommentar um ein brnach zu testen
-        System.out.println("Hunde können auch beißen!");
-        //kleiner Test für die Arbeit
-        System.out.println("Test füt die Arbeit!");
-        System.out.println("Test füt die Arbeit!");
-        System.out.println("Test füt die Arbeit!");
-        System.out.println("Test füt die Arbeit!");
-        System.out.println("Test füt die Arbeit!");
-
-        System.out.println("Shuuuuuuu!");
-
-        System.out.println("Das ist feature2!");
     }
 }
 
