a9ffb3e45143a3d31bdac1f36ce77e8231cd1b89
DarioElpapi <dario.carballeira98@web.de>
2024-08-11 13:37:06 +0200

    code verbessert

--- andereDatei.java
+++ andereDatei.java
@@ -1,8 +1,7 @@
 public class andereDatei {
-    for(int i =0; i<10; i++){
-        System.out.println(i);
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
-    int y=5;
-    int x=6;
-    int y=4;
 }
